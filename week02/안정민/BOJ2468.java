d9b01baac5f945d1d18f3b4d4c92d30efc025c4f
102sae <102chimee@gmail.com>
2024-04-09 13:29:28 +0900

    fix : 오타 수정

--- week02/안정민/BOJ2468.java
+++ week02/안정민/BOJ2468.java
@@ -2,61 +2,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-public class Main {
-    static int[][] graph;
+public class BOJ1303 {
+    static char[][] field;
+    static int n,m;
     static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int rain;
-
-    static int N;
+    static int [] dx = {1,-1,0,0};
+    static int [] dy = {0,0,1,-1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //가로 크기
+        n = Integer.parseInt(st.nextToken());
+        //세로 길이
+        m = Integer.parseInt(st.nextToken());
 
-        graph = new int[N][N];
+        field = new char[m][n];
+        visited = new boolean[m][n];
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                graph[i][j] =Integer.parseInt(st.nextToken());
+        //field 받기
+        for(int i=0; i<m; i++){
+            String line = br.readLine();
+            for(int j=0; j<n; j++){
+                field[i][j] = line.charAt(j);
             }
         }
 
-        int max = 0;
-        for(int R = 0; R< 100; R++){
-            rain = R;
-            visited = new boolean[N][N];
-            int count = 0;
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (graph[i][j] > rain && !visited[i][j]) {
-                        dfs(i, j, rain);
-                        count++;
+        int powerW=0, powerB=0;
+        for(int i =0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(!visited[i][j])
+                {
+                    int power = dfs(i,j,field[i][j]);
+                    if(field[i][j]=='W'){
+                        powerW += power*power;
+                    }
+                    else{
+                        powerB += power*power;
                     }
                 }
             }
-            max = Math.max(max, count);
         }
-
-
-        System.out.println(max);
-
-
+        System.out.println(powerW+" " + powerB);
     }
 
-    private static void dfs(int x, int y, int rain) {
-        if (x < 0 || y < 0 || x >= N || y >= N || graph[x][y] <= rain || visited[x][y]) {
-            return ;
+    private static int dfs(int x, int y, char team) {
+        //그래프를 벗어나거나 다른 팀이면
+        if(x<0  || y<0 || x>=m || y>=n || visited[x][y] || field[x][y] != team){
+            return 0;
         }
         visited[x][y] = true;
-        for(int i = 0; i<4; i++){
-            dfs(x+dx[i],y+dy[i],rain);
+        int power = 1;
+        for(int i =0; i<4; i++)
+        {
+            power += dfs(x+dx[i],y+dy[i], team);
         }
-
+        return power;
     }
-
 }
